523fa383438659254fa06daaeca4d8105ce7eaa2
zh3nis <zhassylbekov@nu.edu.kz>
2025-11-23 01:52:55 +0000

    LDALoss

--- src/lda.py
+++ src/lda.py
@@ -1,6 +1,9 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Function
+from torch.amp import custom_fwd, custom_bwd
 
 
 class LDAHead(nn.Module):
@@ -48,3 +51,64 @@ class LDAHead(nn.Module):
         log_det = self.D * self.log_cov.to(z.dtype)
         log_prior = torch.log_softmax(self.prior_logits, dim=0)
         return log_prior.unsqueeze(0) - 0.5 * (m2 / var + log_det)
+
+
+
+class _GenericLoss(nn.Module):
+    def __init__(self, ignore_index=-100, reduction="elementwise_mean"):
+        assert reduction in ["elementwise_mean", "sum", "none"]
+        self.reduction = reduction
+        self.ignore_index = ignore_index
+        super(_GenericLoss, self).__init__()
+
+    def forward(self, X, target):
+        loss = self.loss(X, target)
+
+        if self.ignore_index >= 0:
+            ignored_positions = target == self.ignore_index
+            size = float((target.size(0) - ignored_positions.sum()).item())
+            loss.masked_fill_(ignored_positions, 0.0)
+        else:
+            size = float(target.size(0))
+        if self.reduction == "sum":
+            loss = loss.sum()
+        elif self.reduction == "elementwise_mean":
+            loss = loss.sum() / size
+        return loss
+
+
+class LDALossFunction(Function):
+    @staticmethod
+    @custom_fwd(device_type="cuda")
+    def forward(ctx, X, target):
+        """
+        X (FloatTensor): n x num_classes
+        target (LongTensor): n, the indices of the target classes
+        """
+        assert X.shape[0] == target.shape[0]
+        p_star = torch.exp(X)
+        loss = X.mean(dim=1) #need to change to normal loss
+        p_star.scatter_add_(1, target.unsqueeze(1), torch.full_like(p_star, -1))
+        ctx.save_for_backward(p_star)
+        return loss
+    
+    @staticmethod
+    @custom_bwd(device_type="cuda")
+    def backward(ctx, grad_output):
+        p_star,  = ctx.saved_tensors 
+        grad = grad_output.unsqueeze(1) * p_star
+        ret = (grad,)
+
+        # pad with as many Nones as needed
+        return ret + (None,) * (1 + 1)
+
+def lda_loss(X, target):
+    return LDALossFunction.apply(X, target)
+
+
+class LDALoss(_GenericLoss):
+    def __init__(self, ignore_index=-100, reduction='elementwise_mean'):
+        super(LDALoss, self).__init__(ignore_index, reduction)
+
+    def loss(self, X, target):
+        return lda_loss(X, target)
